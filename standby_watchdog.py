bf49f7614166ed0e932995bcd987bbb43ec048d7
Jonas Passerini <passuf@gmail.com>
2020-11-05 11:40:36 +0000

    Use logging

--- standby_watchdog.py
+++ standby_watchdog.py
@@ -1,28 +1,30 @@
 import os
 import time
 import subprocess
+import logging
 from datetime import datetime
 
 TARGET_DIR = '/autofs'
 EXPECTED_SHARE_FOLDER = 'vault'
 SLEEP_DURATION = 60  # Time between checks
-STANDBY_COUNT = 15  # Number of checks before disk is disconnected
+STANDBY_COUNT = 5  # Number of checks before disk is disconnected
 POWEROFF_COMMAND = 'uhubctl -l 1-1 -a off -r 100'
 
 def run():
+    logging.info('Starting Standby Watchdog')
     count = 0
     while True:
         is_mounted = check_mount()
 
         if is_mounted:
-            print(f'{datetime.now()} disk is mounted')
+            logging.info(f'Disk is mounted')
             count = 0
         else:
             count += 1
-            print(f'{datetime.now()} disk is not mounted, count: {count}')
+            logging.info(f'Disk is not mounted, count: {count}')
             
             if count >= STANDBY_COUNT:
-                print(f'{datetime.now()} Powering off USB devices')
+                logging.info(f'Powering off USB devices')
                 process = subprocess.Popen(POWEROFF_COMMAND.split(), stdout=subprocess.PIPE)
                 output, error = process.communicate()
                 count = 0
@@ -43,5 +45,6 @@ def check_mount():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='/home/pi/autofs-standby/standby.log', level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s')
     run()
 
